4ae61df07bc7e7cedd81eb024aaacde96db4c731
Davy Decorps <davydecorps@gmail.com>
2018-05-02 23:31:04 +0200

    Added Vehicle class

--- StratPlanner/StratPlanner/Program.cs
+++ StratPlanner/StratPlanner/Program.cs
@@ -10,8 +10,14 @@ namespace StratPlanner
     {
         static void Main(string[] args)
         {
-            
+            uint carNumber = 12;
+            List<Vehicle> carList = new List<Vehicle>() ;
 
+            for (int i=0; i < carNumber; ++i)
+            {
+                Vehicle v = new Vehicle();
+                carList.Add(v);
+            }
         }
     }
 }
---
+++ StratPlanner/StratPlanner/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StratPlanner
+{
+    class Vehicle
+    {
+        static uint nbInstance=0;
+
+        double trackPosition = 100;
+        double absoluteRacePosition = 0;
+
+        uint currentLap;
+
+        uint vehicleID;
+
+        public Vehicle() {
+            ++nbInstance;
+            vehicleID = nbInstance;
+            System.Console.WriteLine("Creation Vehicle n° " + vehicleID);
+        }
+
+        ~Vehicle() {
+            nbInstance--;
+        }
+    }
+}
+
